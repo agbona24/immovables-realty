026fcb9068def984cb16e84ef0550f5246874454
agbonaazeez <softlineazeez123@gmail.com>
2026-03-10 19:38:25 +0100

    Fix Sanity config to use fallback values for env vars

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,8 +1,9 @@
 import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+// Use fallbacks for build-time when env vars might not be available
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'ih6y25kt'
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
 
 export const client = createClient({
